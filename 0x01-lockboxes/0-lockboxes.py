5a083086d077664080ed9fb61a4ec10b64d16228
LeadConsult <106965102+LeadConsult@users.noreply.github.com>
2023-04-28 01:55:23 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,27 +5,27 @@
 
 def canUnlockAll(boxes):
     # Create variables for the current position and unlocked boxes
-    position = 0
-    unlocked = {}
+    current_position = 0
+    unlocked_boxes = {}
 
     # Loop through each box in the list of boxes
     for box in boxes:
         # If the box is empty or the position is 0, mark it as always unlocked
-        if len(box) == 0 or position == 0:
-            unlocked[position] = "always_unlocked"
+        if len(box) == 0 or current_position == 0:
+            unlocked_boxes[current_position] = "always_unlocked"
         
         # Loop through each key in the box
         for key in box:
             # If the key is valid and not the current position, mark it as unlocked
-            if key < len(boxes) and key != position:
-                unlocked[key] = key
+            if key < len(boxes) and key != current_position:
+                unlocked_boxes[key] = key
         
         # If all boxes are unlocked, return True
-        if len(unlocked) == len(boxes):
+        if len(unlocked_boxes) == len(boxes):
             return True
         
         # Move to the next box
-        position += 1
+        current_position += 1
     
     # If not all boxes are unlocked, return False
     return False
