d60e86828ef75337955a8799e15f3b320b974174
LeadConsult <leadconsultnig@gmail.com>
2023-05-04 22:06:08 +0100

    commit

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -16,11 +16,12 @@ int
 """
 
 def minOperations(n):
+    if n <= 1:
+        return 0
+
     operations = 0
-    min_operations = 2
-    while n > 1:
-        while n % min_operations == 0:
-            operations += min_operations
-            n /= min_operations
-        min_operations += 1
+    for i in range(2, n+1):
+        while n % i == 0:
+            operations += i
+            n //= i
     return operations
