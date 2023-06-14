fd727b1546ffbdc4ab1d294a8bb983f16a8fe4f4
LeadConsult <leadconsultnig@gmail.com>
2023-06-14 01:33:21 +0100

    modified

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,35 +6,35 @@ Make change module
 def makeChange(coins, total):
     """
     Given a pile of coins of different values,
-    determine the fewest number of coins
+    determine fewest number of coins
     needed to meet a given amount 'total'
 
     Args:
-        coins (list): a list of the values of the coins in your possession
-        total (number): the target amount
+        coins (list): a list of values of coins in your possession
+        total (number): target amount
 
     Returns:
-        int: the fewest number of coins needed to meet the total
-             or -1 if the total cannot be reached
+        int: fewest number of coins needed to meet total
+             or -1 if total cannot be reached
 
     """
     if total <= 0:
         return 0
 
-    coins.sort(reverse=True)  # Sort the coins in descending order
+    coins.sort(reverse=True)  # Sort coins in descending order
 
     i, ncoins = (0, 0)  # Initialize variables for iteration and coin count
-    cpy_total = total  # Make a copy of the total amount
-    len_coins = len(coins)  # Get the length of the coins list
+    cpy_total = total  # Make a copy of total amount
+    len_coins = len(coins)  # Get length of coins list
 
-    # Iterate over the coins and decrement the total amount
+    # Iterate over coins and decrement total amount
     while i < len_coins and cpy_total > 0:
-        if (cpy_total - coins[i]) >= 0:  # Check if the current coin can be used
-            cpy_total -= coins[i]  # Deduct the coin value from the total amount
-            ncoins += 1  # Increment the coin count
+        if (cpy_total - coins[i]) >= 0:  # Check if current coin can be used
+            cpy_total -= coins[i]  # Deduct coin value from total amount
+            ncoins += 1  # Increment coin count
         else:
-            i += 1  # Move to the next coin if the current coin is too large
+            i += 1  # Move to next coin if current coin is too large
 
-    # Check if the total amount is not fully covered or no coins were used
+    # Check if total amount is not fully covered or no coins were used
     check = cpy_total > 0 and ncoins > 0
-    return -1 if check or ncoins == 0 else ncoins  # Return -1 or the number of coins
+    return -1 if check or ncoins == 0 else ncoins  # Return -1 or no of coins
